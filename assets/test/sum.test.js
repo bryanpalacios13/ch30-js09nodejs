72eaf205a11da8fcd0cfa62724ad36b12886d49b
bryanpalacios13 <bryan.axel1301@gmail.com>
2023-08-18 17:22:18 -0600

    ci: test

---
+++ assets/test/sum.test.js
@@ -0,0 +1,8 @@
+// importar el archivo a probar
+const { sum } = require("../test/sum");
+
+
+// test("Prueba de 1 + 3 = 4", ()=>{});
+test("Prueba de 1 + 3 = 4", ()=>{
+    expect( sum(1,3) ).toBe(4);
+});
